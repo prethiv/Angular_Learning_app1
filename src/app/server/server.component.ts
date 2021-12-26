cb34778bed2eed069343b56a2a4ab94f7514af06
Prethiv <prethiv191@gmail.com>
2021-12-26 17:46:44 +0530

    Dynamic styles

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,8 +10,16 @@ export class ServerComponent{
     serverId:number=10;
     serverStatus:string='Online';
 
+    constructor(){
+        this.serverStatus = Math.random()>0.5?'Online':'Offline';
+    }
+
     getServerStatus(){
         return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus==='Online'?'green':'red';
     }
 
 }
