8a80b6e6af7d6bee74ef229025d60f8a45caf8ca
Prethiv <prethiv191@gmail.com>
2021-12-26 18:10:26 +0530

    ng FOR Tuto

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,11 @@ export class ServersComponent implements OnInit {
   serverName="";
   userName="";
   serverCreated=false;
+  servers=[
+    'Asia',
+    'India',
+    'USA'
+  ]
 
   constructor() {
 
